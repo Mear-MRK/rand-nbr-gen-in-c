537b6d8272dbfd67df2c5bcd19bb16c7d63084e7
Mear M. R. Koochakie <87490118+Mear-MRK@users.noreply.github.com>
2023-03-16 22:43:42 -0600

    Updates

--- lcg/lcg_test.c
+++ lcg/lcg_test.c
@@ -9,34 +9,6 @@
 
 #include "lcg.h"
 
-
-void bytes_to_bits_str_nb(const void *bytes, char *str, size_t nbr_bits) {
-    unsigned char *ch_bytes = (unsigned char*)bytes;
-    size_t n_bytes = nbr_bits/8, r_bits = nbr_bits % 8;
-    for (size_t i = 0; i < n_bytes; i++) {
-        for(unsigned char hot_1 = 0x80U; hot_1 > 0; hot_1 >>= 1) {
-            *str++ = (ch_bytes[i] & hot_1) ? '1' : '0';
-        }
-    }
-    for(unsigned char hot_1 = 1U << (r_bits-1); hot_1 > 0; hot_1 >>= 1) {
-            *str++ = (ch_bytes[n_bytes] & hot_1) ? '1' : '0';
-    }
-    
-    *str = '\0';
-}    
-
-void insert_delimiters(const char *inp_str, char *out_str, unsigned int n, char dlm) {
-    unsigned int i = 0;
-    char r;
-    while( (r = *inp_str++) ) {
-        *out_str++ = r;
-        if ( ++i == n  ) {
-            *out_str++ = dlm;
-            i = 0;
-        }
-    }
-    *out_str = '\0';
-}
     
 void test_lcg_rand(size_t size) { 
     uint32_t r;
@@ -127,30 +99,20 @@ void test_lcg_flt(size_t size) {
 size_t write_rng_bits_to_file(const char *filename, size_t nbr_rnds, uint32_t (*rng_rand)(void), int nbit) {
 
     FILE *sm_file = NULL;
-    if (!(sm_file = fopen(filename, "wb")))
-    {
+    if (!(sm_file = fopen(filename, "wb"))) {
         perror("ERR: ");
         return (size_t)-1;
     }
-    #define N_P 9
-    char str_out[32*N_P+1];
-    memset(str_out, 0, 32*N_P+1);
+
     size_t written = 0;
     unsigned char *buff = (unsigned char *)malloc(nbit);
-    unsigned count = 0;
+    
     for (size_t i = 0; i < nbr_rnds / 8; i++) {
         memset(buff, 0, nbit);
         unsigned off = 0;
         unsigned char *pb = buff;
         for (int j = 0; j < 8; j++) {
-            uint32_t rnd = rng_rand();
-            if (count++ < N_P) {
-                // printf("%08x,", rnd);
-                char bin[33];
-                bytes_to_bits_str_nb((char*)&rnd, bin, nbit);
-                strcat(str_out, bin);
-            }
-                
+            uint32_t rnd = rng_rand();               
             unsigned char *pr = (unsigned char *)&rnd;
             int rem;
             for(rem = nbit; rem > 0; rem -= 8) {
@@ -174,79 +136,80 @@ size_t write_rng_bits_to_file(const char *filename, size_t nbr_rnds, uint32_t (*
         }
         written += fwrite(buff, 1, nbit, sm_file);
     }
-    // puts(str_out);
-    char sp_out[36*N_P+1];
-    insert_delimiters(str_out, sp_out, 8, ',');
-    puts(sp_out);
     
     free(buff);
     fclose(sm_file);
     return written;
 }
 
-size_t test_period(uint32_t v) {
-    size_t period = 0;
-    int flag = 0;
-    uint32_t internal = 0;
-    
-    for (size_t i = 0; i <= 2 * UINT_MAX; i++) {
-        if ( lcg_rand() == v ) {
-            if (! flag ) {
-                internal = lcg_rand_intern_stat();
-                flag = 1;
-                period = i;
-                continue;
-            }
-            if (internal == lcg_rand_intern_stat()) {
-                period = i - period;
-                break;
-            }
-        }
-    }
-    return period;
+size_t write_rng_bytes_to_file(const char *filename, size_t nbr_rnds, uint32_t (*rng_uint32)(void)) {
 
+    FILE *sm_file = NULL;
+    if (!(sm_file = fopen(filename, "wb"))) {
+        perror("ERR: ");
+        return (size_t)-1;
+    }
+    
+    size_t written = 0;
+
+    for (size_t i = 0; i < nbr_rnds; i++) {
+        uint32_t rnd = rng_uint32();
+        written += fwrite(&rnd, 1, 4, sm_file);
+    }
+
+    fclose(sm_file);
+    return written;
 }
 
-size_t test_intern_stat(uint32_t st) {
+size_t intern_stat_period(uint32_t st) {
+    size_t period = 0;
+    int flag = 0;
+    
+    for (size_t i = 0; i <= 2 * UINT32_MAX; i++) {
+        if ( lcg_rand_intern_stat() == st ) {
+            period = i - period;
+            if (flag)
+                break;
+            else
+                flag = 1;
+        }
+        lcg_rand();
+    }
+    return period;
+}
+
+size_t reaching_intern_stat(uint32_t st) {
     for (size_t i = 0; i <= UINT32_MAX; i++) {
         lcg_rand();
         if (lcg_rand_intern_stat() == st)
             return i+1;
     }
-    return (size_t) -1;
+    return 0;
 }
 
-static int nbit = 15;
 
-uint32_t ones(void) {
-    return ((~0U) >> (32-nbit));
-}
-
-uint32_t ozoz(void) {
-    uint32_t out = 0U;
-    for(int i = 0; i < nbit; i+=2 )
-        out |= (1U << i);
-    return out;
-}
-
-int main(int argc, char **argv) {
-    //test_lcg_rand((size_t)UINT32_MAX+1ull);
-    // test_lcg_uint32(2*(size_t)UINT32_MAX);
-    // test_lcg_flt((size_t)UINT32_MAX*2);
-    unsigned v = 11;
-    /* if (argc > 1)
-        nbit = (unsigned) atoi(argv[1]);
-    assert(nbit <= 32);
-    size_t nbr_rnds = ceil(1024*1024.0/nbit/8.0)*8;
-    if (argc > 2)
-        nbr_rnds = (size_t) atol(argv[2]);  */
-    if (argc > 1)
-        v = (unsigned) atoi(argv[1]);
-    // printf("Period of %u is %llu.\n", v, test_period(v));
-    printf("After %llu steps reaching state %u.\n", test_intern_stat(v), v);
-   /* char filename[32];
-    sprintf(filename, "LCG%d.bin", nbit);
-    size_t written = write_rng_bits_to_file(filename, nbr_rnds, rnd, nbit);
-    printf("\n%llu %ubit random numbers generated. %llu bytes written to %s.\n", nbr_rnds, nbit, written, filename); */
+int main() {
+    puts("lcg_rand test:");
+    test_lcg_rand((size_t)UINT32_MAX+1ull);
+    
+    puts("lcg_uint32 test:");
+    test_lcg_uint32(2*(size_t)UINT32_MAX);
+    
+    puts("lcg_flt test:");
+    test_lcg_flt(2*(size_t)UINT32_MAX);
+    
+    uint32_t st = 12345;
+    printf("Period of state %u is %llu.\n", st, intern_stat_period(st));
+    
+    char filename[32];
+    sprintf(filename, "LCG%d.bin", 15);
+    size_t nbr_rnds = 256000;
+    size_t written = write_rng_bits_to_file(filename, nbr_rnds, lcg_rand, 15);
+    printf("%llu 15-bit random numbers generated. %llu bytes written to %s.\n", nbr_rnds, written, filename);
+    sprintf(filename, "LCG%d.bin", 32);
+    nbr_rnds = 128000;
+    written = write_rng_bytes_to_file(filename, nbr_rnds, lcg_uint32);
+    printf("%llu 32-bit random numbers generated. %llu bytes written to %s.\n", nbr_rnds, written, filename);
+    
     return 0;
 }
